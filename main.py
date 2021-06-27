b0e8a192869b1bd3f5105be8155c578fd48d9f8f
alfredobaetti <73269514+alfredobaetti@users.noreply.github.com>
2021-06-27 03:02:44 -0300

    Versión 1
    Midiendo HR y presentando en pantalla

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from process import Process
 from signal_handler import Handler
 import numpy as np
 import fft_filter
@@ -7,34 +8,11 @@ import hr_calculator
 from LE import LE
 from sklearn.manifold import SpectralEmbedding
 import time
+from face_detection import FaceDetection
+from process import Process
 
 freqs_min = 0.8
 freqs_max = 4
-
-def rect_to_bb(rect):
-	# take a bounding predicted by dlib and convert it
-	# to the format (x, y, w, h) as we would normally do
-	# with OpenCV
-	x = rect.left()
-	y = rect.top()
-	w = rect.right() - x
-	h = rect.bottom() - y
-
-	# return a tuple of (x, y, w, h)
-	return (x, y, w, h)
-
-def shape_to_np(shape, dtype="int"):
-	# initialize the list of (x, y)-coordinates
-	coords = np.zeros((shape.num_parts, 2), dtype=dtype)
-
-	# loop over all facial landmarks and convert them
-	# to a 2-tuple of (x, y)-coordinates
-	for i in range(0, shape.num_parts):
-		coords[i] = (shape.part(i).x, shape.part(i).y)
-
-	# return the list of (x, y)-coordinates
-	return coords
-
 
 def get_hr(ROI, fps):
     signal_handler = Handler(ROI)
@@ -67,83 +45,112 @@ if __name__ == '__main__':
     HR = []
     heartrate = 0
     camera_code = 0
-    #capture = cv.VideoCapture(camera_code)
-    capture = cv.VideoCapture('pei_reposo.mp4')
+    capture = cv.VideoCapture(camera_code)
+    #capture = cv.VideoCapture('pei_cardio.mp4')
     fps = capture.get(cv.CAP_PROP_FPS)
-    detector = dlib.get_frontal_face_detector()
-    predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
-    tinicial = time.time_ns()
-    heartrate2=0
+    Process = Process()
+    FaceDetection = FaceDetection()
+
 
     while capture.isOpened():
         ret, frame = capture.read()
-        if not ret:
-            continue
-    
-        grayf = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-        face = detector(grayf, 0)
-        #for face in dects:
-        if len(face) >0:
-            shape = predictor(grayf, face[0])
-            shape = shape_to_np(shape)
 
-            for (a, b) in shape:
-                 cv.circle(frame, (a, b), 1, (0, 0, 255), -1) #draw facial landmarks
+        Process.frame_in = frame
+        Process.run()
 
-            left = face[0].left()
-            right = face[0].right()
-            top = face[0].top()
-            bottom = face[0].bottom()
-            h = bottom - top
-            w = right - left
+        print(np.mean(Process.bpms))
+        
+        frame = Process.frame_out #get the frame to show in GUI
+        f_fr = Process.frame_ROI #get the face to show in GUI
 
-            #roi = frame[top + h // 10 * 2:top + h // 10 * 7, left + w // 9 * 2:left + w // 9 * 8]
+        bpm = Process.bpm #get the bpm change over the time
+        
+        #cv.imshow('frame', frame)
+        if Process.bpms.__len__() >50:
+            if(max(Process.bpms-np.mean(Process.bpms))<5): #show HR if it is stable -the change is not over 5 bpm- for 3s
+                heartrate = np.mean(Process.bpms)
 
-            cv.rectangle(frame,(shape[54][0], shape[29][1]), #draw rectangle on right and left cheeks
-                        (shape[12][0],shape[33][1]), (0,255,0), 0)
-            cv.rectangle(frame, (shape[4][0], shape[29][1]), 
-                    (shape[48][0],shape[33][1]), (0,255,0), 0)
-            
-            ROI1 = frame[shape[29][1]:shape[33][1], #right cheek
-                    shape[54][0]:shape[12][0]]
-                    
-            ROI2 =  frame[shape[29][1]:shape[33][1], #left cheek
-                    shape[4][0]:shape[48][0]]
-
-            #cv.rectangle(frame, (left + w // 9 * 2, top + h // 10 * 3), (left + w // 9 * 8, top + h // 10 * 7), color=(0, 0, 255))
-            #cv.rectangle(frame, (left, top), (left + w, top + h), color=(0, 0, 255))
-            ROI.append(ROI1)
-
-            ROI.append(ROI2)
-
-            if len(ROI) == 50:
-                heartrate = get_hr(ROI, fps)
-                HR.append(heartrate)
-                for i in range(50):
-                    ROI.pop(0)
-
-            cv.imshow('frame', frame)
-            cv.putText(frame, '{:.1f}bps'.format(heartrate2), (50, 300), cv.FONT_HERSHEY_SIMPLEX, 1.2, (255, 255, 255), 2)
-
-            tfinal = time.time_ns()
-            
-            if(tfinal-tinicial > 5000000000):
-                tiempo_transcurrido = tfinal - tinicial
-                tinicial = time.time_ns()
-
-                if(max(HR-np.mean(HR))<30):
-                    cv.putText(frame, '{:.1f}bps'.format(heartrate), (50, 300), cv.FONT_HERSHEY_SIMPLEX, 1.2, (255, 255, 255), 2)
-                    cv.imshow('frame', frame)
-                    heartrate2=heartrate
-                    for c in len(HR):
-                        HR.pop(0)
-            
-
-        else:
-            cv.putText(frame, "No face detected",
-                       (200,200), cv.FONT_HERSHEY_PLAIN, 1.5, (0, 0, 255),2)
-            cv.imshow('frame', frame)
+        cv.putText(frame, '{:.1f}bps'.format(heartrate), (50, 300), cv.FONT_HERSHEY_SIMPLEX, 1.2, (255, 255, 255), 2)
+        cv.imshow('frame', frame)
 
         if cv.waitKey(1) & 0xFF == ord('q'):
             break
+        
+        
+
+
+
+
+
+
+
+        # if not ret:
+        #     continue
+    
+        # grayf = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        # face = detector(grayf, 0)
+        # #for face in dects:
+        # if len(face) >0:
+        #     shape = predictor(grayf, face[0])
+        #     shape = shape_to_np(shape)
+
+        #     for (a, b) in shape:
+        #          cv.circle(frame, (a, b), 1, (0, 0, 255), -1) #draw facial landmarks
+
+        #     left = face[0].left()
+        #     right = face[0].right()
+        #     top = face[0].top()
+        #     bottom = face[0].bottom()
+        #     h = bottom - top
+        #     w = right - left
+
+        #     #roi = frame[top + h // 10 * 2:top + h // 10 * 7, left + w // 9 * 2:left + w // 9 * 8]
+
+        #     cv.rectangle(frame,(shape[54][0], shape[29][1]), #draw rectangle on right and left cheeks
+        #                 (shape[12][0],shape[33][1]), (0,255,0), 0)
+        #     cv.rectangle(frame, (shape[4][0], shape[29][1]), 
+        #             (shape[48][0],shape[33][1]), (0,255,0), 0)
+            
+        #     ROI1 = frame[shape[29][1]:shape[33][1], #right cheek
+        #             shape[54][0]:shape[12][0]]
+                    
+        #     ROI2 =  frame[shape[29][1]:shape[33][1], #left cheek
+        #             shape[4][0]:shape[48][0]]
+
+        #     #cv.rectangle(frame, (left + w // 9 * 2, top + h // 10 * 3), (left + w // 9 * 8, top + h // 10 * 7), color=(0, 0, 255))
+        #     #cv.rectangle(frame, (left, top), (left + w, top + h), color=(0, 0, 255))
+        #     ROI.append(ROI1)
+
+        #     ROI.append(ROI2)
+
+        #     if len(ROI) == 50:
+        #         heartrate = get_hr(ROI, fps)
+        #         HR.append(heartrate)
+        #         for i in range(50):
+        #             ROI.pop(0)
+
+        #     cv.imshow('frame', frame)
+        #     cv.putText(frame, '{:.1f}bps'.format(heartrate2), (50, 300), cv.FONT_HERSHEY_SIMPLEX, 1.2, (255, 255, 255), 2)
+
+        #     tfinal = time.time_ns()
+            
+        #     if(tfinal-tinicial > 5000000000):
+        #         tiempo_transcurrido = tfinal - tinicial
+        #         tinicial = time.time_ns()
+
+        #         if(max(HR-np.mean(HR))<30):
+        #             cv.putText(frame, '{:.1f}bps'.format(heartrate), (50, 300), cv.FONT_HERSHEY_SIMPLEX, 1.2, (255, 255, 255), 2)
+        #             cv.imshow('frame', frame)
+        #             heartrate2=heartrate
+        #             for c in len(HR):
+        #                 HR.pop(0)
+            
+
+        # else:
+        #     cv.putText(frame, "No face detected",
+        #                (200,200), cv.FONT_HERSHEY_PLAIN, 1.5, (0, 0, 255),2)
+        #     cv.imshow('frame', frame)
+
+        # if cv.waitKey(1) & 0xFF == ord('q'):
+        #     break
